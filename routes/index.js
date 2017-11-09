fb628ab2f3ceae15447681c10e5ac1b14a9c1752
Gustavo Gutierrez <imguss@gmail.com>
2017-11-08 22:23:06 -0500

    more github api testing

--- routes/index.js
+++ routes/index.js
@@ -35,4 +35,89 @@ router.post('/users/search', (req, res, next) => {
   });
 });
 
+
+router.get('/repos', (req, res, next) => {
+  res.render('repos');
+});
+
+router.post('/repos', (req, res, next) => {
+
+  const gitUser = req.body.gitUser;
+
+  const options = {
+    url: `${githubUrl}/users/${gitUser}/repos?per_page=100`,
+    body: JSON.stringify({
+      client_id: clientId,
+      client_secret: clientSecret
+    }),
+    json: true,
+    headers: {
+      'User-Agent': 'imguss'
+    }
+  };
+
+  request.get(options, (err, response, repos) => {
+    if (err) { return next(err); }
+
+    const languageObj = {};
+
+    // added an element so that i can loop through it.
+    const languages = [];
+
+    // iterate over each repo
+    repos.forEach( (repo) => {
+      cnt = 1;
+      console.log('INSIDE REPO FOR EACH');
+
+      // iterate of each language in the language array
+      languages.forEach( (language) => {
+        console.log('INSIDE LANGUAGE FOR EACH');
+
+        console.log('LANGUAGES ARRAY SECOND IF', languages);
+        // if languages array is empty
+        if (languages.length === 0) {
+          languages.push(language);
+          languageObj[language] = 1;
+          return;
+        }
+
+        // if repo's language and element in language array are the SAME
+        // AND the language doesn't exist in the object
+        if (repo.language === language && !languageObj[language]) {
+          console.log('LANGUAGES ARRAY SECOND IF', languages);
+          // add the language as a key and make the value 1
+           languageObj[language] = 1;
+           return;
+        }
+
+        // if the repo's language and the element in language are are the SAME
+        // AND the language DOES exist in the object
+        if (repo.language === language && languageObj[language]) {
+          console.log('LANGUAGES ARRAY THIRD IF', languages);
+          // add one to the value
+          languageObj[language] += 1;
+          return;
+        }
+
+        if (cnt >= languages.length) {
+          languages.push(language);
+          console.log('LANGUAGES ARRAY FOURTH IF', languages);
+          languageObj[language] = 1;
+          cnt += 1;
+          return;
+        }
+
+
+      });
+      // languages.push(repo.language);
+      // console.log('LANGUAGES ARRAY',languages);
+    });
+
+    console.log('LANGUAGEOBJ~~~~~~', languageObj);
+
+    // console.log('REPOS~~~~', repos);
+    res.render('show-repos-view', { repos, languages: languageObj } );
+  });
+});
+
 module.exports = router;
