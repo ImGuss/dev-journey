ce94cf230834cfd916004ef87ae23026defa48d8
Gustavo Gutierrez <imguss@gmail.com>
2017-11-18 17:00:17 -0500

    get repos and sort. remove no language repos

--- routes/index.js
+++ routes/index.js
@@ -57,9 +57,13 @@ router.post('/repos', (req, res, next) => {
   };
 
   request.get(options, (err, response, repos) => {
-    // console.log('REPOS ARRAY!!!~~~~~', repos);
-    let languages = repos;
-    languages.sort( (a, b) => {
+
+    repos.forEach( (repo, index) => {
+      if (!repo.language) {
+        repos.splice(index, 1);
+      }
+    });
+    repos.sort( (a, b) => {
       if (a.created_at < b.created_at) {
         return -1;
       }
@@ -71,94 +75,9 @@ router.post('/repos', (req, res, next) => {
       return 0;
     });
 
-    // console.log('LANGUAGES ARRAY!!!~~~~', languages);
 
-    res.render('show-repos-view', { repos: repos, languages: languages });
+    res.render('show-repos-view', { repos: repos });
   });
 });
-
-
-
-// router.post('/repos', (req, res, next) => {
-//
-//   const gitUser = req.body.gitUser;
-//
-//   const options = {
-//     url: `${githubUrl}/users/${gitUser}/repos?per_page=100`,
-//     body: JSON.stringify({
-//       client_id: clientId,
-//       client_secret: clientSecret
-//     }),
-//     json: true,
-//     headers: {
-//       'User-Agent': 'imguss'
-//     }
-//   };
-//
-//   request.get(options, (err, response, repos) => {
-//     if (err) { return next(err); }
-//
-//
-//     const languageObj = {};
-//
-//     // added an element so that i can loop through it.
-//     const languages = [];
-//
-//     // iterate over each repo
-//     repos.forEach( (repo) => {
-//       let elementFound;
-//       let cnt = 1;
-//
-//       // if languages array is empty
-//       if (languages.length === 0) {
-//         languages.push(repo.language);
-//         languageObj[repo.language] = 1;
-//         return;
-//       }
-//
-//       // iterate of each language in the language array
-//       languages.forEach( (language) => {
-//
-//         // if repo's language and element in language array are the SAME
-//         // AND the language doesn't exist in the object
-//         if (repo.language === language && !languageObj[language]) {
-//           // add the language as a key and make the value 1
-//            languageObj[language] = 1;
-//            return;
-//         }
-//
-//         // if the repo's language and the element in language are are the SAME
-//         // AND the language DOES exist in the object
-//         if (repo.language === language && languageObj[language]) {
-//           // add one to the value
-//           languageObj[language] += 1;
-//           return;
-//         }
-//
-//         elementFound = false;
-//
-//         // if goes through full array and no elements match
-//         if (cnt === languages.length) {
-//           elementFound = false;
-//           cnt += 1;
-//           return;
-//         }
-//
-//       }); // end languages.forEach
-//
-//       if (elementFound === false) {
-//         languages.push(repo.language);
-//         languageObj[repo.language] = 1;
-//         elementFound = true;
-//         return;
-//       }
-//       // console.log('LANGUAGES ARRAY',languages);
-//       // console.log('LANGUAGE OBJ', languageObj);
-//       console.log(repos);
-//     }); // end repos.forEach
-//
-//     res.render('show-repos-view', { repos, languages: languageObj } );
-//   });
-// });
 
 module.exports = router;
